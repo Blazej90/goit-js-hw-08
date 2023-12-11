b3b71d08044b7227784f6ec80bba156748bcba3f
Błażej Bartoszewski <blazejbart@gmail.com>
2023-12-11 18:37:42 +0100

    feat: part 1 and part2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('ul.gallery');
+let lightboxInstance;
+
+const images = galleryItems.map(item => {
+  return `<div class="gallery__item">
+      <a class="gallery__item-link" href="${item.original}">
+          <img
+              class="gallery__image"
+              src="${item.preview}"
+              data-source="${item.original}"
+              alt="${item.description}"
+          />
+      </a>
+  </div>`;
+});
+
+galleryContainer.insertAdjacentHTML('beforeend', images.join(''));
+
+lightbox = new SimpleLightbox('.gallery__item-link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
