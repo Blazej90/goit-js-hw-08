1903685eeb4cc87ae748ed9c5f1a5eeb680e4d3d
Błażej Bartoszewski <blazejbart@gmail.com>
2023-12-12 15:08:18 +0100

    feat: add if (time !== null && !== undefined)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,5 +12,7 @@ player.on(
 
 window.addEventListener('DOMContentLoaded', () => {
   const time = localStorage.getItem('videoplayer-current-time');
-  player.setCurrentTime(time);
+  if (time !== null && time !== undefined) {
+    player.setCurrentTime(time);
+  }
 });
